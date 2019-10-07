1a427151277b1e0edcc52e8b539561c1dcc3b14e
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-07 15:24:00 +0200

    first step done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-
+import Contact from './components/Contact.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Contact
+        name= 'Clinton Hayes'
+        avatar= 'https://randomuser.me/api/portraits/men/30.jpg'
+        online= 'true'
+      />
+     <Contact
+        name= 'Miriam Webb'
+        avatar= 'https://randomuser.me/api/portraits/women/27.jpg'
+        online= 'false'
+      />
+      <Contact
+        name= 'Wayne Rose' 
+        avatar= 'https://randomuser.me/api/portraits/men/83.jpg'
+        online= 'true'
+      />   
+      </div>
   );
 }
-
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Contact.css';
 
-function Contact () {
-  
+function Contact(props){
+  return (
+    <div className="Contact">
+    <img className='avatar' src={props.avatar} alt={props.name}></img>
+   <div>
+    <h4>{props.name}</h4>
+    <status> {props.status}
+    <p>{props.online ? "Online" : 'Offline'}</p>
+    </status>
+    </div>
+  </div>
+);
 }
+
+export default Contact;
