85a5b401a563883428c430652706f5ca38503598
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-07 16:39:51 +0200

    finish version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import Contact from './components/Contact.js'
+import Contact from './components/Contact'
+
 
 function App() {
   return (
     <div className="App">
+
       <Contact
         name= 'Clinton Hayes'
         avatar= 'https://randomuser.me/api/portraits/men/30.jpg'
         online= {true}
       />
+
      <Contact
         name= 'Miriam Webb'
         avatar= 'https://randomuser.me/api/portraits/women/27.jpg'
         online= {false}
       />
+
       <Contact
         name= 'Wayne Rose' 
         avatar= 'https://randomuser.me/api/portraits/men/83.jpg'
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props){
+function Contact (props) {
+
   return (
-    <div className="Contact">
+
+  <div className="Contact">
     <img className='avatar' src={props.avatar} alt={props.name}></img>
-   <div>
-    <h4>{props.name}</h4>
-    <div>{props.status}</div>
-    <p>{props.online ? "Online" : 'Offline'}</p>
+    <div>
+      <h4>{props.name}</h4>
+    <div className="status">
+      <div className= {props.online ? 'status-online' : 'status-offline'}>
+    </div>
+      <p>{props.online ? "Online" : 'Offline'} </p>
+    </div>
     </div>
   </div>
+
 );
 }
 
